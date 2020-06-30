442b1d95b266c97548e291e39f45975a849b7489
Gescarduca <44589787+Gescarduca@users.noreply.github.com>
2020-06-30 11:27:28 +0100

    sync

--- aeda_ds/aed_ds/dictionaries/hash_table.py
+++ aeda_ds/aed_ds/dictionaries/hash_table.py
@@ -1,115 +1,25 @@
-# from .tad_dictionary import Dictionary
-# from ..exceptions import NoSuchElementException, DuplicatedKeyException
-# from ..lists.singly_linked_list import SinglyLinkedList
-
-from aed_ds.dictionaries.tad_dictionary import Dictionary
-from aed_ds.dictionaries.item import Item
-from aed_ds.lists.singly_linked_list import SinglyLinkedList
-from aed_ds.exceptions import DuplicatedKeyException
-from aed_ds.exceptions import NoSuchElementException
-import ctypes
-
+from .tad_dictionary import Dictionary
+from ..exceptions import NoSuchElementException, DuplicatedKeyException
+from ..lists.singly_linked_list import SinglyLinkedList
 
 class HashTable(Dictionary):
     def __init__(self, size=101):
-        self.limit = size
-        self.current_number_of_elements = 0
-        self.vec = (ctypes.py_object * size)()
-        for a in range(self.limit):
-            self.vec[a] = SinglyLinkedList()
+        pass
 
-    def hashing_func(self, key):
-        return sum([ord(c) for c in key]) % self.limit
+    def size(self): pass
 
-    def has_key(self,key,list_to_search):
-        place_to_search = list_to_search
-        piter = place_to_search.iterator()
-        while piter.has_next():
-            obj_to_compare = piter.next()
-            if key == obj_to_compare.get_key():
-                return obj_to_compare
-            else:
-                return False
+    def is_full(self): pass
 
-    def size(self):
-        return self.current_number_of_elements
+    def get(self, k): pass
 
-    def is_full(self):
-        if self.current_number_of_elements == self.limit:
-            return True
+    def insert(self, k, v): pass
 
-    def get(self, k):
-        hash_key = self.hashing_func(k)
-        slot = self.vec[hash_key]
-        element = self.has_key(k,slot)
-        if not element:
-            raise NoSuchElementException
-        else:
-            return element.get_value()
+    def update(self, k, v): pass
 
-    def insert(self, k, v):
-        hash_key = self.hashing_func(k)
-        key_exists = False
-        slot = self.vec[hash_key]
-        item = Item(k, v)
-        #need to check if the key in items already exists
-        if slot.is_empty():
-            slot.insert_first(item)
-        else:
-            true_key = self.has_key(k,slot)
-            if true_key == False:
-                slot.insert_first(item)
-            else:
-                raise DuplicatedKeyException
-        self.current_number_of_elements+=1
+    def remove(self, k): pass
 
-    def update(self, k, v):
-        hash_key = self.hashing_func(k)
-        slot = self.vec[hash_key]
-        if not self.has_key(k,slot):
-            raise NoSuchElementException
-        else:
-            item_to_update = self.has_key(k,slot)
-            item_to_update.set_value(v)
+    def keys(self): pass
 
-    def remove(self, k):
-        hash_key = self.hashing_func(k)
-        slot = self.vec[hash_key]
-        list_iter = slot.iterator()
-        position = 0
-        while list_iter.has_next():
-            if k == list_iter.next().get_key():
-                slot.remove(position)
-            else:
-                position+=1
-        self.current_number_of_elements-=1
+    def values(self): pass
 
-    def keys(self):
-        my_list = []
-        for bucket in self.vec:
-            bucket_iter = bucket.iterator()
-            while bucket_iter.has_next():
-                element = bucket_iter.next().get_key()
-                my_list.append(element)
-        return my_list
-
-    def values(self):
-        my_list = []
-        for bucket in self.vec:
-            bucket_iter = bucket.iterator()
-            while bucket_iter.has_next():
-                element = bucket_iter.next().get_value()
-                my_list.append(element)
-        return my_list
-
-    def items(self):
-        my_list_of_buckets = []
-        for bucket in self.vec:
-            bucket_iter = bucket.iterator()
-            my_list_per_bucket = []
-            while bucket_iter.has_next():
-                key = bucket_iter.next().get_key()
-                value = bucket_iter.next().get_value()
-                kv = (key,value)
-                my_list_per_bucket.append(kv)
-            my_list_of_buckets.append(my_list_per_bucket)
+    def items(self): pass
--- aeda_ds/aed_ds/lists/doubly_linked_list.py
+++ aeda_ds/aed_ds/lists/doubly_linked_list.py
@@ -7,102 +7,106 @@ class DoublyLinkedList(SinglyLinkedList):
     def __init__(self):
         SinglyLinkedList.__init__(self)
 
-    def insert_first(self, element): 
-        new_node = DoubleListNode(element, self.head, None)
-        if not self.head:
-            
-            self.tail = new_node
+    def insert_first(self, element):
+        self.head = DoubleListNode(element, self.head, None)
+        if self.tail is None:
+            self.tail = self.head
+        self.num_elements += 1
+
+    def insert_last(self, element):
+        if self.num_elements == 0:
+            self.head = DoubleListNode(element, None, None)
+            self.tail = self.head
+            self.num_elements += 1
         else:
-            self.head.set_previous(new_node)
-        self.head = new_node
-        self.num_elements += 1
-        
-    def insert_last(self, element): 
-        new_node = DoubleListNode(element, None, self.tail)
-        if not self.head: 
-            self.head = new_node
-        else:    
-            self.tail.set_next(new_node)
-        self.tail = new_node
-        self.num_elements += 1
+            previous = self.tail
+            node = DoubleListNode(element, None, previous)
+            self.tail = node
+            previous.set_next(self.tail) 
+            self.num_elements += 1
 
     def insert(self, element, position):
-        if position < 0 or position > self.size():
+        if position > self.num_elements or position < 0:
             raise InvalidPositionException()
-        elif position == 0:
-            return self.insert_first(element)
-        elif position == self.size():
-            return self.insert_last(element)
-        prev_node = self.head
-        cur_node = self.head
-        idx = 0
-        while prev_node:
-            if position == idx:
-                new_node = DoubleListNode(element, cur_node, prev_node)
-                prev_node.set_next(new_node)
-                cur_node.set_previous(new_node)
-                self.num_elements += 1
-                break
-            prev_node = cur_node
-            cur_node = cur_node.get_next()
-            idx += 1
+        else: 
+            if position == 0:
+                self.insert_first(element)
+            elif position == self.size():
+                self.insert_last(element)
+            else:
+                current = self.head
+                index = 0
+                while True:
+                    if index == position:
+                        previous = current.get_previous()
+                        node = DoubleListNode(element, current, previous)
+                        previous.set_next(node)
+                        current.set_previous(node)
+                        break
+                    current = current.get_next()
+                    index += 1
 
-    def remove_first(self): 
+                self.num_elements += 1
+
+    def remove_first(self):
         if self.num_elements == 1:
-            old_node = self.head
-            self.make_empty()
-            return old_node.get_element()
+            old_head = self.head
+            self.head = None
+            self.tail = None
+            self.num_elements = 0
+            return old_head.get_element()
         elif self.num_elements == 0:
             raise EmptyListException()
-        old_head = self.head
-        self.head = self.head.get_next()
-        self.head.set_previous(None)
-        self.num_elements -= 1
-        return old_head.get_element()
+        else:
+            old_head = self.head
+            temp = self.head
+            temp = self.head.get_next()
+            self.head = temp
+            self.head.set_previous(None)
+            self.num_elements -= 1
+            return old_head.get_element()
 
     def remove_last(self):
-        if self.size() == 0:
-            raise EmptyListException()        
-        elif self.size() == 1:
-            old_node = self.head
-            self.make_empty()
-            return old_node.get_element()
-        elif self.size() == 2:
-            self.head.set_next(None)
-            old_node = self.head
-            self.tail.set_previous(None)
-            self.tail = self.head
-            self.num_elements -= 1
-            return old_node.get_element()
+        if self.num_elements == 1:
+            old_tail = self.tail
+            self.head = None
+            self.tail = None
+            self.num_elements = 0
+            return old_tail.get_element()
+        elif self.num_elements == 0:
+            raise EmptyListException()
         else:
-            old_node = self.tail
-            new_tail = self.tail.get_previous()
-            new_tail.set_next(None)
-            self.tail.set_previous(None)
-            self.tail = new_tail
+            old_tail = self.tail
+            var = self.tail.get_previous()
+            self.tail = var
+            self.tail.set_next(None)
             self.num_elements -= 1
-            return old_node.get_element()
+            return old_tail.get_element()
 
     def remove(self, position):
-        if position < 0 or position > self.size() - 1:
+        if position < 0 or position > (self.size() - 1) :
             raise InvalidPositionException()
-        elif position == 0:
-            return self.remove_first()
-        elif position == self.size() - 1:
-            return self.remove_last()
-        cur_node = self.head
-        idx = 0
-        while cur_node:
-            if idx == position:
-                prev_node = cur_node.get_previous()
-                next_node = cur_node.get_next()
-                prev_node.set_next(next_node)
-                next_node.set_previous(prev_node)
-                self.num_elements -= 1
-                return cur_node.get_element()
-            cur_node = cur_node.get_next()
-            idx += 1
+        else:
+            if position == 0:
+                return self.remove_first()
+            elif position == (self.size()-1):
+                return self.remove_last()
+            else:
+                current = self.head
+                index = 0
+                while True:
+                    if index == position:
+                        old_node = current
+                        temp = current.get_next()
+                        temp_previous = current.get_previous()
+                        temp.set_previous(temp_previous)
+                        temp_previous.set_next(temp)
+                        self.num_elements -= 1
+                        return old_node.get_element()
 
-    def iterator(self): 
+                    current = current.get_next()
+                    index += 1
+
+    def iterator(self):
         return DoublyLinkedListIterator(self)
                 
